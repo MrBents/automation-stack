30e4390343e145829f272e0caad448c8c4bf605e
Max Bentata <maxbentata@Maxs-MacBook-Pro.local>
2018-05-26 17:47:08 -0400

    Added dependecies

--- sauron/client.js
+++ sauron/client.js
@@ -3,3 +3,19 @@ var body_parser = require("body-parser");
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
+
+app.use(express.static(__dirname));
+app.use(body_parser.json()); //
+app.use(body_parser.urlencoded({
+    extended: false
+})); // to allow for req.body
+
+io.on("connection", (socket) => {
+    console.log("socket connected");
+});
+
+
+var server = http.listen(3000, () => {
+    console.log("server is listening on port", server.address().port);
+});
